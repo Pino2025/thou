ec56151fc797b23629f7c11b75c759512e08ee1d
Pino2025 <phouthasin2005@gmail.com>
2025-06-25 17:41:06 +0700

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+function page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>ພໍ່ ບັກໄກວັນ ຊື່ ສົມດີ</h1>
+    <div>
+            <h1>ພໍ່ ບັກໄກວັນ ຊື່ ສົມດີ</h1>
     </div>
-  );
+  )
 }
+
+export default page
