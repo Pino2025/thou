a9eccd8a91906ff3fc5385440a28412352d6b13b
Pino2025 <phouthasin2005@gmail.com>
2025-06-25 17:52:50 +0700

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <div>
-      <h1>Hello Next.js</h1>
+      <h1>ພໍ່ ບັກໄກວັນ ຊື່ ສົມດີ</h1>
       <Image src="/logo.png" width={100} height={100} alt="Logo" />
     </div>
   );
